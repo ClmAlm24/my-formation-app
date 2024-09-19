eb4b933b0b88943cdd8f99adfdba419c05125fd6
ClmAlm24 <colombealimi@gmail.com>
2024-09-19 17:12:37 +0200

    first comimit

--- src/js/script.js
+++ src/js/script.js
@@ -14,7 +14,15 @@ loginBtn.addEventListener('click', () => {
 registerBtn.addEventListener('click', () => {
     inscriptionSection.classList.remove('hidden');
     connexionSection.classList.add('hidden');
+    registerBtn.classList.add('btn')
     registerBtn.classList.add('text-primary');
     loginBtn.classList.remove('text-primary');
     loginBtn.classList.add('text-secondary');
 });
+
+
+// Import des modules nécessaires
+import { Ripple, Input, initTWE } from 'tw-elements';
+
+// Initialisation de tw-elements avec les composants requis
+initTWE({ Ripple, Input });
